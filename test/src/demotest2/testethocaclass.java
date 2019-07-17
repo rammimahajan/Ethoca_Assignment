dce1d4f35548d909931d7721f3d03c36da0ba42d
RammiMahajan <rammi.mahajan89@gmail.com>
2019-07-16 22:20:35 -0400

    Test Commit

---
+++ test/src/demotest2/testethocaclass.java
@@ -0,0 +1,12 @@
+package demotest2;
+
+public class testethocaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+	}
+
+}
